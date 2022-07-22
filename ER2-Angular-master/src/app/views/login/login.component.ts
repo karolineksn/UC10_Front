d31cfa1aa152b8f0c477ee889f67e0d6d31caab8
karolineksn <99697119+karolineksn@users.noreply.github.com>
2022-07-21 23:33:44 -0300

    Finalização

---
+++ ER2-Angular-master/src/app/views/login/login.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Route, Router } from '@angular/router';
+import { User } from 'src/app/models/user';
+import { LoginService } from 'src/app/services/login.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private loginService: LoginService, private router: Router) { }
+
+  ngOnInit(): void {
+  }
+  loginModel = new User('', '');
+  
+  userModel = new User("", "") 
+
+  onSubmit(){
+    console.log(this.userModel)
+  }
+
+}
+
+
+onSubmit(){
+  console.log(this.loginModel)
+
+  this.loginService.login(this.loginModel).subscribe((response)=> {
+    console.log(response)
+  })
+}
+
+}
